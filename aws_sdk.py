3bce9f64e9662905689ab7b6d1ed002469394fb2
Mauricio Cuello <mdavid_cuello@javeriana.edu.co>
2024-01-17 19:59:28 +0100

    Function for create key pair and function for security group"
    Still and issue with the group ID of the SG

--- aws_sdk.py
+++ aws_sdk.py
@@ -2,55 +2,115 @@ import boto3
 from botocore.exceptions import ClientError
 import paramiko
 import time
+import subprocess
+import os
 
-ec2 = boto3.client('ec2')
+"""
+result = subprocess.run(["dir"], shell=True, capture_output=True, text=True)
+
+print(result.stdout)"""
+
+
+def create_key_pair(key_name):
+    try:
+        key_pair = ec2.create_key_pair(KeyName=key_name)
+        private_key = key_pair["KeyMaterial"]
+    except ClientError as e:
+        print(
+            "Key pair already exists, proceeding to eliminate it and create a new one"
+        )
+        response = ec2.delete_key_pair(KeyName=key_name)
+        key_pair = ec2.create_key_pair(KeyName=key_name)
+        private_key = key_pair["KeyMaterial"]
+    return private_key
+
+
+def security_group_create(ec2):
+    # Create security group allowing SSH access
+    response = ec2.create_security_group(
+        GroupName="ssh-access", Description="Security group with SSH access"
+    )
+
+    security_group_id = response["GroupId"]
+    print("Security group ID: ", security_group_id)
+    # Authorize SSH access
+    ec2.authorize_security_group_ingress(
+        GroupId=security_group_id,
+        IpPermissions=[
+            {
+                "IpProtocol": "tcp",
+                "FromPort": 22,
+                "ToPort": 22,
+                "IpRanges": [{"CidrIp": "0.0.0.0/0"}],
+            }
+        ],
+    )
+
+    return security_group_id
+
+
+ec2 = boto3.client("ec2", region_name="us-east-2")
 
 # Create key pair
-key_pair = ec2.create_key_pair(KeyName='VPN_KEY')
-private_key = key_pair['KeyMaterial']
+if os.path.exists("keys/") and not ec2.describe_key_pairs()["KeyPairs"]:
+    if not os.path.exists("vpn_key.pem"):
+        private_key = create_key_pair("vpn_key")
+else:
+    # Create directory for keys
+    subprocess.run(["mkdir", "keys"], capture_output=True, text=True)
+    subprocess.run(["touch", "/keys/vpn_key.pem"], capture_output=True, text=True)
+    private_key = create_key_pair("vpn_key")
 
 # Save private key to file
-with open('vpn_key.pem', 'w') as key_file:
-    key_file.write(private_key) 
+with open("keys/vpn_key.pem", "w") as key_file:
+    key_file.write(private_key)
 
 # Set user data script
-with open("/bash_scripts/server_VPN.sh") as file:
+with open("bash_scripts/server_VPN.sh") as file:
     user_data = file.read()
-    
+
+# Create security group
+security_group_id = security_group_create(ec2)
+print("Security group ID outside: ", security_group_id)
 
 # Launch EC2 instance
 instances = ec2.run_instances(
-    ImageId='ami-0748d13ffbc370c2b', # Ubuntu Linux 22.04
-    InstanceType='t2.micro',
-    KeyName='VPN_KEY',
+    ImageId="ami-05fb0b8c1424f266b",  # Ubuntu Linux 22.04
+    InstanceType="t2.micro",
+    KeyName="vpn_key",
     UserData=user_data,
     MaxCount=1,
-    MinCount=1
+    MinCount=1,
+    SecurityGroups=[security_group_id],
 )
 
-instance_id = instances['Instances'][0]['InstanceId']
+instance_id = instances["Instances"][0]["InstanceId"]
 
 # Wait for instance to run
-waiter = ec2.get_waiter('instance_status_ok') 
+print("Waiting for instance to run...")
+waiter = ec2.get_waiter("instance_status_ok")
 waiter.wait(InstanceIds=[instance_id])
 
 # Get public DNS name
-instance = ec2.describe_instances(InstanceIds=[instance_id])['Reservations'][0]['Instances'][0]
-public_dns = instance['PublicDnsName']
+instance = ec2.describe_instances(InstanceIds=[instance_id])["Reservations"][0][
+    "Instances"
+][0]
+print(f"Instance: {instance} is running")
+public_dns = instance["PublicDnsName"]
 
-# Connect via SSH 
+# Connect via SSH
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 while True:
-  try:
-    print(f"Connecting to {public_dns}") 
-    ssh.connect(hostname=public_dns, username="ec2-user", key_filename="my_key.pem")
-    print("Connected!")
-    break
-  except:
-    print("Connection failed, retrying...")
-    time.sleep(10)
+    try:
+        print(f"Connecting to {public_dns}")
+        ssh.connect(hostname=public_dns, username="ec2-user", key_filename="my_key.pem")
+        print("Connected!")
+        break
+    except:
+        print("Connection failed, retrying...")
+        time.sleep(10)
 
 # Interact with EC2 instance
 stdin, stdout, stderr = ssh.exec_command("uname -a")
