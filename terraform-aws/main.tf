8052f39cb1f80fa65b2ad9b1775b62336e378aaf
Mauricio Cuello <mdavid_cuello@javeriana.edu.co>
2024-02-08 21:04:33 +0100

    Terraform initiation

---
+++ terraform-aws/main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0c20d88b0021158c6"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.vpn_subnet.id
+  associate_public_ip_address = true
+  tags = {
+    Name = "server-docker"
+  }
+}
+
+resource "aws_vpc" "vpn_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "tf_vpn_vpc"
+  }
+}
+
+resource "aws_subnet" "vpn_subnet" {
+
+  vpc_id = aws_vpc.vpn_vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "tf_vpn_subnet"
+  }
+}
+
+resource "aws_security_group" "ssh" {
+  name        = "ssh-sg"
+  description = "Allow ssh access"
+  vpc_id      = aws_vpc.vpn_vpc.id
+
+  ingress {
+    description = "SSH from everywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh-sg"
+  }
+}
