2410c1f21931949e773ca6b0c435901af2fc85a0
Mauricio Cuello <mdavid_cuello@javeriana.edu.co>
2024-02-17 23:14:32 +0100

    Relocate files

--- small-ca-server/execution_ca.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run --name ca-server -it -v step:/home/step \
-  -p 9000:9000 \
-    -e "DOCKER_STEPCA_INIT_NAME=Smallstep" \
-    -e "DOCKER_STEPCA_INIT_DNS_NAMES=localhost,$(hostname -f)" \
-    -e "DOCKER_STEPCA_INIT_REMOTE_MANAGEMENT=true" \
-    smallstep/step-ca
